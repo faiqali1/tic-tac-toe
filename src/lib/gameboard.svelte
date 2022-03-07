ba2af9ae60a04c3c1bba47b8b85747edafc95d6d
Syed Faiq Ali Faisal <faiqali2010@hotmail.com>
2022-03-07 12:12:07 +0500

    working rest button

--- src/lib/gameboard.svelte
+++ src/lib/gameboard.svelte
@@ -1,101 +1,97 @@
 <script>
-  import bestMove from "../scripts/AI_logic" 
-  let list= ["", "", "", "", "", "", "", "", ""];
-  let humanWin= false;
-  let botWin= false;
-  let draw= false;
-
+  import bestMove from "../scripts/AI_logic";
+  let list = ["", "", "", "", "", "", "", "", ""];
+  let humanWin = false;
+  let botWin = false;
+  let draw = false;
 
   function addX(id) {
-			if (humanWin || botWin) {
-				return;
-			}
-			// @ts-ignore
-			if (!list[id] == "") {
-				return;
-			}
-			if (list[id] == "") {
-				list[id] = "X";
-			}
+    if (humanWin || botWin) {
+      return;
+    }
+    // @ts-ignore
+    if (!list[id] == "") {
+      return;
+    }
+    if (list[id] == "") {
+      list[id] = "X";
+    }
 
-			computerAnswer();
-			updateWinState();
-      
-		}
+    computerAnswer();
+    updateWinState();
+  }
 
-    function reset() {
-			list = ["", "", "", "", "", "", "", "", ""];
-			humanWin = false;
-			botWin = false;
-			draw = false;
-		}
+  function reset() {
+    list = ["", "", "", "", "", "", "", "", ""];
+    humanWin = false;
+    botWin = false;
+    draw = false;
+  }
 
-    // function addValue() {
-		// 	console.log("addValue is accessed ");
-		// 	MakeMove(message, list);
-		// }
+  // function addValue() {
+  // 	console.log("addValue is accessed ");
+  // 	MakeMove(message, list);
+  // }
 
-    function computerAnswer() {
-			let output = 0;
-			output = bestMove(list);
-			MakeMove(output, list);
-			updateWinState();
-			console.log("The best move to make is:" + output);
-		}
+  function computerAnswer() {
+    let output = 0;
+    output = bestMove(list);
+    MakeMove(output, list);
+    updateWinState();
+    console.log("The best move to make is:" + output);
+  }
 
-    function updateWinState() {
-			// TODO: add to all required places
-			humanWin = checkWin("X", list);
-			botWin = checkWin("O", list);
-			if (!list.includes("") && !botWin && !humanWin) {
-				draw = true;
-			}
-		}
+  function updateWinState() {
+    // TODO: add to all required places
+    humanWin = checkWin("X", list);
+    botWin = checkWin("O", list);
+    if (!list.includes("") && !botWin && !humanWin) {
+      draw = true;
+    }
+  }
 
-    
-function MakeMove(num, list) {
-	let humanWin = false;
-	let botWin = false;
+  function MakeMove(num, list) {
+    let humanWin = false;
+    let botWin = false;
 
-	if (list[num] == "") {
-		humanWin = checkWin("X", list);
-		botWin = checkWin("O", list);
-		if (humanWin) {
-			return {
-				HasPlayerWon: humanWin,
-				HasBotWon: botWin,
-			};
-		}
-		list[num] = "O"; // assignment
-	}
+    if (list[num] == "") {
+      humanWin = checkWin("X", list);
+      botWin = checkWin("O", list);
+      if (humanWin) {
+        return {
+          HasPlayerWon: humanWin,
+          HasBotWon: botWin,
+        };
+      }
+      list[num] = "O"; // assignment
+    }
 
-	// final check
-	humanWin = checkWin("X", list);
-	botWin = checkWin("O", list);
+    // final check
+    humanWin = checkWin("X", list);
+    botWin = checkWin("O", list);
 
-	return {
-		HasPlayerWon: humanWin,
-		HasBotWon: botWin,
-	};
-}
+    return {
+      HasPlayerWon: humanWin,
+      HasBotWon: botWin,
+    };
+  }
 
-function checkWin(WinChar, list) {
-	function checkLine(x, y, z) {
-		return list[x] == WinChar && list[y] == WinChar && list[z] == WinChar;
-	}
+  function checkWin(WinChar, list) {
+    function checkLine(x, y, z) {
+      return list[x] == WinChar && list[y] == WinChar && list[z] == WinChar;
+    }
 
-	return (
-		checkLine(0, 3, 6) ||
-		checkLine(1, 4, 7) ||
-		checkLine(2, 5, 8) ||
-		checkLine(0, 1, 2) ||
-		checkLine(3, 4, 5) ||
-		checkLine(6, 7, 8) ||
-		checkLine(0, 4, 8) ||
-		checkLine(2, 4, 6)
-	);
-}
-
+    return (
+      checkLine(0, 3, 6) ||
+      checkLine(1, 4, 7) ||
+      checkLine(2, 5, 8) ||
+      checkLine(0, 1, 2) ||
+      checkLine(3, 4, 5) ||
+      checkLine(6, 7, 8) ||
+      checkLine(0, 4, 8) ||
+      checkLine(2, 4, 6)
+    );
+  }
 </script>
 
 <body>
@@ -110,36 +106,44 @@ function checkWin(WinChar, list) {
           tag="tr"
         >
           <tr>
-            <td id="0" on:click={()=> addX(0)}>{list[0]}</td>
-            <td id="1" class="vert" on:click={()=> addX(1)}>{list[1]}</td>
-            <td id="2" on:click={()=> addX(2)}>{list[2]}</td>
+            <td id="0" on:click={() => addX(0)}>{list[0]}</td>
+            <td id="1" class="vert" on:click={() => addX(1)}>{list[1]}</td>
+            <td id="2" on:click={() => addX(2)}>{list[2]}</td>
           </tr>
           <tr>
-            <td id="3" class="hori" on:click={()=> addX(3)}>{list[3]}</td>
-            <td id="4" class="vert hori" on:click={()=> addX(4)}>{list[4]}</td>
-            <td id="5" class="hori" on:click={()=> addX(5)}>{list[5]}</td>
+            <td id="3" class="hori" on:click={() => addX(3)}>{list[3]}</td>
+            <td id="4" class="vert hori" on:click={() => addX(4)}>{list[4]}</td>
+            <td id="5" class="hori" on:click={() => addX(5)}>{list[5]}</td>
           </tr>
           <tr>
-            <td id="6" on:click={()=> addX(6)}>{list[6]}</td>
-            <td id="7" class="vert" on:click={()=> addX(7)}>{list[7]}</td>
-            <td id="8" on:click={()=> addX(8)}>{list[8]}</td>
+            <td id="6" on:click={() => addX(6)}>{list[6]}</td>
+            <td id="7" class="vert" on:click={() => addX(7)}>{list[7]}</td>
+            <td id="8" on:click={() => addX(8)}>{list[8]}</td>
           </tr>
         </transition-group>
       </table>
     </div>
-    <div
-      class="winner-stage animate__animated animate__slideInLeft"
-      v-if="humanWin || botWin || draw"
-    >
-      <div v-if="humanWin" class="Win-stage">You Win!!!</div>
-      <div v-if="botWin" class="Win-stage">Computer Wins!!!</div>
-      <div v-if="draw" class="Win-stage">Draw , Better Luck next time</div>
-      <div class="mx-auto my-4" style="width: 120px">
-        <button type="button" class="btn btn-success mx-auto">
-          Play Again 🤞
-        </button>
+    {#if humanWin || botWin || draw}
+      <div class="winner-stage animate__animated animate__slideInLeft">
+        {#if humanWin}
+          <div v-if="humanWin" class="Win-stage">You Win!!!</div>
+        {/if}
+
+        {#if botWin}
+          <div v-if="botWin" class="Win-stage">Computer Wins!!!</div>
+        {/if}
+
+        {#if draw}
+        <div v-if="draw" class="Win-stage">Draw , Better Luck next time</div>
+        {/if}
+
+        <div class="mx-auto my-4" style="width: 120px">
+          <button type="button" on:click={reset} class="btn btn-success mx-auto">
+            Play Again 🤞
+          </button>
+        </div>
       </div>
-    </div>
+    {/if}
   </div>
 </body>
 
