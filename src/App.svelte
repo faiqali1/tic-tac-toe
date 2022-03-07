3a7208dd85d67b46b02ffddad06c6bd8d6ce5ae0
Syed Faiq Ali Faisal <faiqali2010@hotmail.com>
2022-03-07 11:07:42 +0500

    added sass and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,11 @@
 <script>
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+  import Gameboard from './lib/gameboard.svelte';
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <Gameboard/>
 </main>
 
 <style>
---
+++ src/lib/gameboard.svelte
@@ -0,0 +1,91 @@
+<script>
+ let list = [0];
+
+</script>
+
+<body>
+    <div class="container-fluid">
+        <h1 class="animate__animated animate__bounce">Tic Tac Toe</h1>
+        <div class="game-board my-5">
+          <table>
+            <transition-group
+              name="custom-classes-transition"
+              enter-active-class="animate__animated animate__bounceIn"
+              leave-active-class="animate__animated animate__bounceOutRight"
+              tag = "tr"
+            >
+              <tr>
+                <td id="0"  v-on:click="addX(0)">{list[0]}</td>
+                <td id="1" class="vert" v-on:click="addX(1)">{list[1]}</td>
+                <td id="2" v-on:click="addX(2)">{list[2]}</td>
+              </tr>
+              <tr>
+                <td id="3" class="hori" v-on:click="addX(3)">{list[3]}</td>
+                <td id="4" class="vert hori" v-on:click="addX(4)">{list[4]}</td>
+                <td id="5" class="hori" v-on:click="addX(5)">{list[5]}</td>
+              </tr>
+              <tr>
+                <td id="6" v-on:click="addX(6)">{list[6]}</td>
+                <td id="7" class="vert" v-on:click="addX(7)">{list[7]}</td>
+                <td id="8" v-on:click="addX(8)">{list[8]}</td>
+              </tr>
+            </transition-group>
+          </table>
+        </div>
+        <div
+          class="winner-stage animate__animated animate__slideInLeft"
+          v-if="humanWin || botWin || draw"
+        >
+          <div v-if="humanWin" class="Win-stage">You Win!!!</div>
+          <div v-if="botWin" class="Win-stage">Computer Wins!!!</div>
+          <div v-if="draw" class="Win-stage">Draw , Better Luck next time</div>
+          <div class="mx-auto my-4" style="width: 120px">
+            <button  type="button" class="btn btn-success mx-auto">
+              Play Again 🤞
+            </button>
+          </div>
+        </div>
+      </div>
+</body>
+
+
+
+
+<style type="text/scss">
+@import url("https://fonts.googleapis.com/css2?family=Varela+Round&display=swap");
+
+h1 {
+  text-align: center;
+}
+td {
+  width: 100px;
+  height: 100px;
+}
+table {
+  margin: 5px auto;
+
+  td {
+    font-size: 60px;
+    text-align: center;
+    font-family: "Varela Round", sans-serif;
+  }
+}
+.vert {
+  border-left: 2px solid black;
+  border-right: 2px solid black;
+}
+.hori {
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+}
+
+.winner-stage {
+  margin: 5em auto;
+  .Win-stage {
+    font-size: 50px;
+    text-align: center;
+    font-family: "Varela Round", sans-serif;
+  }
+}
+
+</style>
